580fea54cc86eaef8c6ca81126e28c36016a95e8
busraozdmir <ozdemirbu20@itu.edu.tr>
2024-11-24 11:46:47 +0300

    Add templates, styles, and Flask app updates
    - Added home.html, layout.html, and teams.html
    
    - Updated main.css for styling
    
    - Created views.py for Flask routes
    
    - Modified __init__.py and run.py for app setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
+import views
+
 def create_app():
     app = Flask(__name__)
-
-    @app.route("/")
-    def home_page():
-        return "Welcome to the website for the Baseball History!"
+    app.add_url_rule(rule="/", view_func=views.home_page)
+    app.add_url_rule(rule="/teams", view_func=views.teams_page)
 
     return app
---
+++ app/views.py
@@ -0,0 +1,8 @@
+from flask import render_template
+
+
+def home_page():
+    return render_template("home.html")
+
+def movies_page():
+    return render_template("teams.html")
